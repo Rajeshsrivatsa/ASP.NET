2d77053a34f93afa46032601ab941f0fabc99c7f
Rajeshsrivatsa <omsaishree@gmail.com>
2017-07-19 19:30:52 +0530

    ASP.NET Files
    MVC Sample

---
+++ GitHubAsp/GitHubAsp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GitHubAsp.MvcApplication" Language="C#" %>
---
+++ GitHubAsp/GitHubAsp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitHubAsp.Startup))]
+namespace GitHubAsp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
